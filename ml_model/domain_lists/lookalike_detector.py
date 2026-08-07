9ae0facda3ca5b33e19e0e0a72b693a9c9972e56
Pathum Siyambalapitiya <ckpathumck@gmail.com>
2026-08-07 10:55:52 +0530

    add character normalization mappings

--- ml_model/domain_lists/lookalike_detector.py
+++ ml_model/domain_lists/lookalike_detector.py
@@ -1,3 +1,30 @@
 import unicodedata
 from urllib.parse import urlparse
 import domain_lists.whitelist as _whitelist
+
+
+# Cyrillic characters visually identical to Latin
+CYRILLIC_TO_LATIN = {
+    "а": "a",
+    "е": "e",
+    "о": "o",
+    "р": "p",
+    "с": "c",
+    "х": "x",
+    "і": "i",
+    "ј": "j",
+    "ѕ": "s",
+    "ԁ": "d",
+}
+
+# Digit substitutions used by Method B for typosquatting
+DIGIT_TO_LETTER = {
+    "0": "o",
+    "1": "l",
+    "3": "e",
+    "4": "a",
+    "5": "s",
+    "6": "g",
+    "7": "t",
+    "8": "b",
+}
